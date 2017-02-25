d20b438100499fbe98d4022c78cec659a8c54d8c
BlackPhreaker <ph@blackphreaker.ru>
2017-02-25 13:39:59 +0300

    Update loop_scan.sh

--- loop_scan.sh
+++ loop_scan.sh
@@ -4,7 +4,7 @@
 #pragma compile(ProductName, loop_scan.sh)
 #pragma compile(FileVersion, 0.0.0)
 #pragma compile(ProductVersion, 0.0.0)
-#pragma compile(FileDescription, IMSI_Catcher GSM Utility)
+#pragma compile(FileDescription, GSM Utility)
 #pragma compile(Comments, Program made by BlackPhreaker)
 #pragma compile(LegalCopyright, Copyright © 1991-2017 BlackPhreaker (BlackPhreaker))
 #pragma compile(CompanyName, BlackPhreaker Tech Company Almaty)
