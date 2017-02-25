b111a9c1c0b6cf6059b3fd04c05d1e0e97a0dcd2
BlackPhreaker <ph@blackphreaker.ru>
2017-02-25 11:58:29 +0300

    Update IMSI_Catcher_Ver1.0.7.py

--- IMSI_Catcher_Ver1.0.7.py
+++ IMSI_Catcher_Ver1.0.7.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#======================================================================================
+#pragma compile(ProductName, IMSI_Catcher_Ver1.0.7.py)
+#pragma compile(FileVersion, 1.0.7)
+#pragma compile(ProductVersion, 1.0.7)
+#pragma compile(FileDescription, IMSI_Catcher GSM Utility)
+#pragma compile(Comments, Program made by BlackPhreaker)
+#pragma compile(LegalCopyright, Copyright © 1991-2017 BlackPhreaker (BlackPhreaker))
+#pragma compile(CompanyName, BlackPhreaker Tech Company Almaty)
+#pragma compile(Sign, BlackPhreaker)
+#pragma compile(Date, 25.02.2017)
+#======================================================================================
 
