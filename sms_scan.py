69bbf6abaf33e5bc01b38e842ee622da647e4f4a
BlackPhreaker <ph@blackphreaker.ru>
2017-02-25 12:12:44 +0300

    Create sms_scan.py

---
+++ sms_scan.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#======================================================================================
+#pragma compile(ProductName, sms_scan.py)
+#pragma compile(FileVersion, 1.0.0)
+#pragma compile(ProductVersion, 1.0.0)
+#pragma compile(FileDescription, IMSI_Catcher GSM Utility)
+#pragma compile(Comments, Program made by BlackPhreaker)
+#pragma compile(LegalCopyright, Copyright © 1991-2017 BlackPhreaker (BlackPhreaker))
+#pragma compile(CompanyName, BlackPhreaker Tech Company Almaty)
+#pragma compile(Sign, BlackPhreaker)
+#pragma compile(Date, 00.02.2017)
+#======================================================================================
