88539ea61a7d756a52575fb76879d523fac5b235
BlackPhreaker <ph@blackphreaker.ru>
2017-08-21 09:44:21 +0300

    Update sms_scan.py

--- sms_scan.py
+++ sms_scan.py
@@ -7,7 +7,7 @@
 #pragma compile(FileDescription, GSM Utility)
 #pragma compile(Comments, Program made by BlackPhreaker)
 #pragma compile(LegalCopyright, Copyright © 1991-2017 BlackPhreaker (BlackPhreaker))
-#pragma compile(CompanyName, BlackPhreaker Tech Company Almaty)
+ 
 #pragma compile(Sign, BlackPhreaker)
 #pragma compile(Date, 00.02.2017)
 #======================================================================================
