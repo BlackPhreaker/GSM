ba49fc4d3219e343b7bbafd67cad2a0bb3502d4a
BlackPhreaker <ph@blackphreaker.ru>
2017-08-25 10:32:45 +0300

    Create rssi_dbm_to_ditance.py

---
+++ rssi_dbm_to_ditance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#======================================================================================
+#pragma compile(ProductName, rssi_dbm_to_distance.py)
+#pragma compile(FileVersion, 0.0.2)
+#pragma compile(ProductVersion, 0.0.2)
+#pragma compile(FileDescription, GSM Utility)
+#pragma compile(Comments, Program made by BlackPhreaker)
+#pragma compile(LegalCopyright, Copyright © 1991-2017 BlackPhreaker (BlackPhreaker))
+#pragma compile(CompanyName, BlackPhreaker Tech Company Almaty)
+#pragma compile(Sign, BlackPhreaker)
+#pragma compile(Date, 25.08.2017)
+#======================================================================================
